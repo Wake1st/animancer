31069a4bbb9d7e63df031442c0aaa79fdb9f0f0b
Wake1st <joeltwakefield@gmail.com>
2024-08-13 14:25:04 -0600

    box selection

--- src/inputs.rs
+++ src/inputs.rs
@@ -1,19 +1,36 @@
-use bevy::prelude::*;
+use bevy::{color::palettes::tailwind::GRAY_100, math::vec3, prelude::*};
 
-use crate::{movement::UnitMovement, selectable::UnitSelection};
+use crate::{
+    movement::UnitMovement,
+    selectable::{BoxSelection, UnitSelection},
+};
 
 pub struct InputPlugin;
 
 impl Plugin for InputPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, handle_click);
+        app.add_systems(Update, (handle_click, draw_box_selection).chain())
+            .insert_resource(BoxSelector {
+                selecting: false,
+                start: Vec2::ZERO,
+                current: Vec2::ZERO,
+            });
     }
+}
+
+#[derive(Resource)]
+struct BoxSelector {
+    selecting: bool,
+    start: Vec2,
+    current: Vec2,
 }
 
 fn handle_click(
     mouse_button_input: Res<ButtonInput<MouseButton>>,
     camera: Query<(&Camera, &GlobalTransform)>,
     windows: Query<&Window>,
+    mut box_selector: ResMut<BoxSelector>,
+    mut box_selection_writer: EventWriter<BoxSelection>,
     mut selection_writer: EventWriter<UnitSelection>,
     mut movement_writer: EventWriter<UnitMovement>,
 ) {
@@ -24,10 +41,61 @@ fn handle_click(
         .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
         .map(|ray| ray.origin.truncate())
     {
-        if mouse_button_input.just_pressed(MouseButton::Left) {
+        if mouse_button_input.pressed(MouseButton::Left) {
+            if box_selector.selecting == false {
+                box_selector.selecting = true;
+                box_selector.start = pos;
+            } else {
+                box_selector.current = pos;
+            }
+        } else if mouse_button_input.just_released(MouseButton::Left) && box_selector.selecting {
+            let mut box_selection = BoxSelection {
+                left: 0.,
+                top: 0.,
+                right: 0.,
+                bottom: 0.,
+            };
+
+            if box_selector.start.x < box_selector.current.x {
+                box_selection.left = box_selector.start.x;
+                box_selection.right = box_selector.current.x;
+            } else {
+                box_selection.left = box_selector.current.x;
+                box_selection.right = box_selector.start.x;
+            }
+
+            if box_selector.start.y < box_selector.current.y {
+                box_selection.top = box_selector.current.y;
+                box_selection.bottom = box_selector.start.y;
+            } else {
+                box_selection.top = box_selector.start.y;
+                box_selection.bottom = box_selector.current.y;
+            }
+
+            box_selection_writer.send(box_selection);
+
+            box_selector.selecting = false;
+            box_selector.start = Vec2::ZERO;
+            box_selector.current = Vec2::ZERO;
+        } else if mouse_button_input.just_pressed(MouseButton::Left) {
             selection_writer.send(UnitSelection { pos });
         } else if mouse_button_input.just_pressed(MouseButton::Right) {
             movement_writer.send(UnitMovement { pos });
         }
+    }
+}
+
+fn draw_box_selection(box_selector: Res<BoxSelector>, mut gizmos: Gizmos) {
+    if box_selector.selecting {
+        gizmos.linestrip(
+            [
+                vec3(box_selector.start.x, box_selector.start.y, 0.0),
+                vec3(box_selector.current.x, box_selector.start.y, 0.0),
+                vec3(box_selector.current.x, box_selector.current.y, 0.0),
+                vec3(box_selector.start.x, box_selector.current.y, 0.0),
+                vec3(box_selector.start.x, box_selector.start.y, 0.0),
+            ],
+            GRAY_100,
+        );
     }
 }
--- src/selectable.rs
+++ src/selectable.rs
@@ -6,8 +6,9 @@ pub struct SelectablePlugin;
 
 impl Plugin for SelectablePlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, select_unit)
-            .add_event::<UnitSelection>();
+        app.add_systems(Update, (select_unit, box_select_units))
+            .add_event::<UnitSelection>()
+            .add_event::<BoxSelection>();
     }
 }
 
@@ -33,6 +34,35 @@ fn select_unit(
             global_transform.translation().y,
         );
         if unit_pos.distance(unit_selection.pos) < unit.size {
+            selectable.selected = true;
+        }
+    }
+}
+
+#[derive(Event)]
+pub struct BoxSelection {
+    pub left: f32,
+    pub top: f32,
+    pub right: f32,
+    pub bottom: f32,
+}
+
+fn box_select_units(
+    mut reader: EventReader<BoxSelection>,
+    mut query: Query<(&GlobalTransform, &mut Selectable), With<Selectable>>,
+) {
+    for box_selection in reader.read() {
+        let (global_transform, mut selectable) = query.get_single_mut().unwrap();
+
+        let unit_pos = vec2(
+            global_transform.translation().x,
+            global_transform.translation().y,
+        );
+        if unit_pos.x > box_selection.left
+            && unit_pos.x < box_selection.right
+            && unit_pos.y > box_selection.bottom
+            && unit_pos.y < box_selection.top
+        {
             selectable.selected = true;
         }
     }
